0d7e9a47468e6c689e2a37ee0d11cc1bfc497891
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2023-10-11 23:27:43 +0800

    -Implemented fall zone

---
+++ Mario Prototype Game/Assets/GameClear/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameEvent Lost;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Lost.Raise();
+            Time.timeScale = 0f;
+        }
+    }
+}
