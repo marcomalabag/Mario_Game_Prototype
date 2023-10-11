5a160771f835d0d6908b2dd8f38e7ad42b8c0e4b
marcomalabag <46800870+marcomalabag@users.noreply.github.com>
2023-10-11 14:57:27 +0800

    -Added and fixed camera follow

---
+++ Mario Prototype Game/Assets/Player/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject Camera;
+
+    [SerializeField]
+    private Vector3 OffSet;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowObject();
+    }
+
+    public void FollowObject()
+    {
+        Vector3 cameraPosition = transform.position + OffSet;
+        Camera.transform.position = cameraPosition;
+
+    }
+}
